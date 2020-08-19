fb2ba3565051adcb75524f64bf54507801466c47
Patrick Freinschlag <patrick.freinschlag@gmail.com>
2020-08-19 17:17:44 +0200

    fixes

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ require_once 'templates/nav.php';
                             setlocale(LC_ALL, 'de_AT');
                             ?>
                             <h5 class="card-title"><?php echo $event->title; ?></h5>
-                            <p class="card-text"><?php echo strftime("%a, %d.%m.%Y", strtotime($event->date . " " . $event->time)); ?></p>
+                            <p class="card-text"><?php echo strftime("%d.%m.%Y %H:%M", strtotime($event->date . " " . $event->time)); ?> Uhr</p>
                             <a href="termine.php" class="btn btn-primary">See more
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
